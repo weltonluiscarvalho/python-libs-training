28b23def3fa6ec9aa90a3a001f9d45de4723e68e
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-07-13 21:30:38 -0300

    testando bytes

--- builtin-libs/main.py
+++ builtin-libs/main.py
@@ -1,7 +1,7 @@
 nome: str = "We&&p€𔐖lton"
 
 
-numero: int = 29
+# numero: int = 29
 
 # print(ascii(nome))
 # print(bool(nome), bool(numero))
@@ -13,4 +13,18 @@ numero: int = 29
 # print(numero.to_bytes(length=4, signed=True, byteorder='big'))
 
 # print(int.from_bytes(b'\x00\x00\x00\x1d', byteorder='little'))
-print(int.from_bytes([1, 2], byteorder='little'))
+# print(int.from_bytes([1, 2], byteorder='little'))
+
+numero1 = 46437
+numero2 = 215
+
+n1b = numero1.to_bytes()
+n2b = numero2.to_bytes()
+
+print(n1b)
+print(n2b)
+
+# print(bytt)
+# print(type(bytt))
+# print(bytt.hex())
+# print(bytt.count(25))
