e91e898fd80388bc38d1030c5ae95bed8eabcdf0
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-07-16 21:09:42 -0300

    undestanding some new concepts

--- cryptographic/main.py
+++ cryptographic/main.py
@@ -8,6 +8,24 @@ my_hash_hexdigest = my_hash.hexdigest()
 # print(my_hash.block_size)
 # print(type(my_hash))
 
-print(my_hash_digest)
-print(my_hash_hexdigest)
-print(type(my_hash_digest))
+# print(my_hash_digest)
+# print(my_hash_hexdigest)
+# print(type(my_hash_digest))
+
+
+my_new_hash = hashlib.new('sha256')
+my_new_hash.update(b'crypto\n')
+print(my_new_hash.hexdigest())
+# print(my_new_hash.hexdigest())
+
+
+# print(my_new_hash.hexdigest())
+# print(my_new_hash.digest_size)
+# print(my_new_hash.block_size)
+# print(hashlib.algorithms_available)
+# print(hashlib.algorithms_guaranteed)
+
+with open('cryptographic/file.txt', 'rb') as file:
+    hash_file = hashlib.file_digest(file, 'sha-256')
+    
+print(hash_file.hexdigest())
