32b00bf7f2e257d09578ca731a81eb0cd6b55731
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-07-28 21:54:37 -0300

    extract box to a class

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,19 +1,24 @@
 from tkinter import *
 from tkinter import ttk
 
+class AtributoDominioBox:
+    def __init__(self, parent):
+        frame = ttk.Frame(parent)
+        frame.grid()
+        categoria_label = ttk.Label(frame, text="Categoria")
+        categoria_label.grid(column=0, row=0)
+        dominio_categoria_label = ttk.Label(frame, text="Dominio Categoria")
+        dominio_categoria_label.grid(column=1, row=0)
+
+        valor_categoria_combo_box = StringVar(value="Nome")
+        categoria_combo_box = ttk.Combobox(frame, values=["Nome", "Idade", "Altura"], textvariable=valor_categoria_combo_box)
+        categoria_combo_box.grid(column=0, row=1)
+
+        valor_dominio_categoria_combo_box = StringVar(value="Jason")
+        dominio_categoria_combo_box = ttk.Combobox(frame, values=["Jason", "Michael", "Peter", "Paul"], textvariable=valor_dominio_categoria_combo_box)
+        dominio_categoria_combo_box.grid(column=1, row=1)
+
 root = Tk()
-frame = ttk.Frame(root)
-frame.grid()
-categoria_label = ttk.Label(frame, text="Categoria")
-categoria_label.grid(column=0, row=0)
-dominio_categoria_label = ttk.Label(frame, text="Dominio Categoria")
-dominio_categoria_label.grid(column=1, row=0)
-
-valor_categoria_combo_box = StringVar(value="Nome")
-categoria_combo_box = ttk.Combobox(frame, values=["Nome", "Idade", "Altura"], textvariable=valor_categoria_combo_box)
-categoria_combo_box.grid(column=0, row=1)
-
-valor_dominio_categoria_combo_box = StringVar(value="Jason")
-dominio_categoria_combo_box = ttk.Combobox(frame, values=["Jason", "Michael", "Peter", "Paul"], textvariable=valor_dominio_categoria_combo_box)
-dominio_categoria_combo_box.grid(column=1, row=1)
+atributo_dominio_box = AtributoDominioBox(root)
+atributo_dominio_box = AtributoDominioBox(root)
 root.mainloop()
