4d24ce10f36b0a8c0710fff55165a346b5005ce7
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-07-31 20:53:31 -0300

    change between two screens

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,35 +1,9 @@
 from tkinter import *
 from tkinter import ttk
-from tela_inicial import TelaInicial, TelaCadastrarHabilidades, GerenciadorTelas
-
-frames = {}
-
-def mostrar_tela(nome_tela):
-    frame = frames[nome_tela]
-    frame.tkraise()
-
-
-class AtributoDominioBox:
-    def __init__(self, parent):
-        frame = ttk.Frame(parent)
-        frame.grid()
-        frame['relief'] = 'sunken'
-        frame['borderwidth'] = 2
-        frame['padding'] = 10
-        categoria_label = ttk.Label(frame, text="Categoria")
-        categoria_label.grid(column=0, row=0)
-        dominio_categoria_label = ttk.Label(frame, text="Dominio Categoria")
-        dominio_categoria_label.grid(column=1, row=0)
-
-        valor_categoria_combo_box = StringVar(value="Nome")
-        categoria_combo_box = ttk.Combobox(frame, values=["Nome", "Idade", "Altura"], textvariable=valor_categoria_combo_box)
-        categoria_combo_box.grid(column=0, row=1)
-
-        valor_dominio_categoria_combo_box = StringVar(value="Jason")
-        dominio_categoria_combo_box = ttk.Combobox(frame, values=["Jason", "Michael", "Peter", "Paul"], textvariable=valor_dominio_categoria_combo_box)
-        dominio_categoria_combo_box.grid(column=1, row=1)
+from tela_inicial import GerenciadorTelas
 
 root = Tk()
+gerenciador_telas = GerenciadorTelas(root)
 root.option_add('*tearOff', FALSE)
 menubar = Menu(root)
 root['menu'] = menubar
@@ -41,19 +15,4 @@ menubar.add_cascade(menu=menu_atendimento, label="Atendimento")
 
 menu_atendimento.add_command(label="Pendencias")
 menu_atendimento.add_command(label="Habilidades")
-# atributo_dominio_box = AtributoDominioBox(root)
-# atributo_dominio_box = AtributoDominioBox(root)
-
-for tela in (TelaInicial, TelaCadastrarHabilidades):
-    frame = tela(root)
-    frames[tela.__name__] = frame
-    frame.grid(row=0, column=0, sticky="nsew")
-
-mostrar_tela("TelaInicial")
-
-gerenciador_telas = GerenciadorTelas(root)
-gerenciador_telas.atualizar_lista_telas()
-
-# tela_inicial = TelaInicial(root)
-# tela_inicial.grid()
 root.mainloop()
--- tkinter/tela_inicial.py
+++ tkinter/tela_inicial.py
@@ -2,32 +2,28 @@ from tkinter import *
 from tkinter import ttk
 
 
-class GerenciadorTelas(ttk.Frame):
+class GerenciadorTelas:
 
-    # Como o Widget precisa saber o pai no momento da criacao, o gerenciador de telas deve criar o widget, ai fica a questão
-    # como o gerenciador de telas, trocara a tela atual? Ele destruira a tela atual, e criara outra? Ou ele apenas coloca a outra
-    # sobreposta? Ou ele apenas retira a visibilidade de uma tela e mostra a de tras?
     def __init__(self, parent) -> None:
-        super().__init__(parent)
         self.telas = dict()
-        self.tela_atual = None
 
-    def mudar_tela(self, tela):
-        self.tela_atual = tela
+        for ClasseTela in (TelaInicial, TelaCadastrarHabilidades):
+            objeto_tela = ClasseTela(parent, self)
+            self.telas[ClasseTela.__name__] = objeto_tela
+            objeto_tela.grid(row=0, column=0, sticky="nsew")
 
-    def adicionar_tela_lista(self, nome_tela, tela):
-        self.telas[nome_tela] = tela
+        self.alterar_tela_atual('TelaInicial')
 
-    def remover_tela_lista(self, nome_tela):
-        self.telas.pop(nome_tela)
-
-    def atualizar_lista_telas(self):
-        print(self.grid_info())
+    def alterar_tela_atual(self, nome_tela):
+        if nome_tela in self.telas.keys():
+            self.tela_atual = self.telas[nome_tela]
+            self.tela_atual.tkraise()
 
 class TelaInicial(ttk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, gerenciador):
         super().__init__(parent)
 
+        self.gerenciador = gerenciador
         self.botao_cadastrar_habilidades = ttk.Button(self, text="Cadastrar Habilidades", command=self.cadastrar_habilidades)
         self.botao_cadastrar_habilidades.grid(row=0, pady=10)
 
@@ -36,6 +32,7 @@ class TelaInicial(ttk.Frame):
 
     def cadastrar_habilidades(self):
         print("Cadastrando habilidades")
+        self.gerenciador.alterar_tela_atual('TelaCadastrarHabilidades')
 
     def consultar_habilidades(self):
         print("Consultando habilidades...")
@@ -43,9 +40,10 @@ class TelaInicial(ttk.Frame):
 
 
 class TelaCadastrarHabilidades(ttk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, gerenciador):
         super().__init__(parent)
 
+        self.gerenciador = gerenciador
         self.label_nome_habilidade = ttk.Label(self, text="Nome Habilidade")
         self.label_nome_habilidade.grid(row=0, column=0)
 
@@ -59,4 +57,30 @@ class TelaCadastrarHabilidades(ttk.Frame):
         self.variable_entry_descricao_habilidade = StringVar()
         self.entry_descricao_habilidade = ttk.Entry(self, textvariable=self.variable_entry_descricao_habilidade)
         self.entry_descricao_habilidade.grid(row=3, column=0)
+          
+        self.botao_consultar_habilidades = ttk.Button(self, text="Voltar para tela inicial", command=self.voltar_tela_inicial)
+        self.botao_consultar_habilidades.grid(row=4, pady=10)
+
+    def voltar_tela_inicial(self):
+        self.gerenciador.alterar_tela_atual('TelaInicial')
+
+class AtributoDominioBox:
+    def __init__(self, parent):
+        frame = ttk.Frame(parent)
+        frame.grid()
+        frame['relief'] = 'sunken'
+        frame['borderwidth'] = 2
+        frame['padding'] = 10
+        categoria_label = ttk.Label(frame, text="Categoria")
+        categoria_label.grid(column=0, row=0)
+        dominio_categoria_label = ttk.Label(frame, text="Dominio Categoria")
+        dominio_categoria_label.grid(column=1, row=0)
+
+        valor_categoria_combo_box = StringVar(value="Nome")
+        categoria_combo_box = ttk.Combobox(frame, values=["Nome", "Idade", "Altura"], textvariable=valor_categoria_combo_box)
+        categoria_combo_box.grid(column=0, row=1)
+
+        valor_dominio_categoria_combo_box = StringVar(value="Jason")
+        dominio_categoria_combo_box = ttk.Combobox(frame, values=["Jason", "Michael", "Peter", "Paul"], textvariable=valor_dominio_categoria_combo_box)
+        dominio_categoria_combo_box.grid(column=1, row=1)
 
