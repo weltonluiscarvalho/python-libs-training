c98ed47b23b096461ceb1d62fb95ebeb0c20b219
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-08-04 20:15:49 -0300

    add CadastrarTecido screen

--- tkinter/tela_inicial.py
+++ tkinter/tela_inicial.py
@@ -3,6 +3,41 @@ from tkinter import ttk
 import db
 from datetime import datetime
 
+class TelaCadastrarTecido(ttk.Frame):
+    def __init__(self, parent, gerenciador):
+        super().__init__(parent)
+
+        self.gerenciador = gerenciador
+        self.label_nome_tecido = ttk.Label(self, text="Nome do tecido")
+        self.label_nome_tecido.grid(row=0, column=0)
+
+        self.variable_entry_nome_tecido = StringVar()
+        self.entry_nome_tecido = ttk.Entry(self, textvariable=self.variable_entry_nome_tecido)
+        self.entry_nome_tecido.grid(row=1, column=0)
+
+        self.label_descricao_tecido = ttk.Label(self, text="Descricao do tecido")
+        self.label_descricao_tecido.grid(row=2, column=0)
+
+        self.variable_entry_descricao_tecido = StringVar()
+        self.entry_descricao_tecido = ttk.Entry(self, textvariable=self.variable_entry_descricao_tecido)
+        self.entry_descricao_tecido.grid(row=3, column=0)
+
+        self.botao_cadastrar_tecido = ttk.Button(self, text="Cadastrar", command=self.cadastrar_tecido)
+        self.botao_cadastrar_tecido.grid(row=4, pady=10)
+          
+        self.botao_voltar = ttk.Button(self, text="Voltar para tela inicial", command=self.voltar_tela_inicial)
+        self.botao_voltar.grid(row=5, pady=10)
+
+    def cadastrar_tecido(self):
+        nome = self.variable_entry_nome_tecido.get()
+        descricao = self.variable_entry_descricao_tecido.get()
+        data_inclusao = datetime.now().strftime("%d/%m/%Y")
+        db.insert_tecido(nome, descricao, data_inclusao)
+        print("Tecido Cadastrado com sucesso!")
+
+    def voltar_tela_inicial(self):
+        self.gerenciador.alterar_tela_atual('TelaInicial')
+
 class TelaCadastrarTipoLavagem(ttk.Frame):
     def __init__(self, parent, gerenciador):
         super().__init__(parent)
@@ -58,7 +93,6 @@ class TelaCadastrarTipoVestimenta(ttk.Frame):
     def cadastrar_tipo_vestimenta(self):
         descricao = self.variable_entry_nome_tipo_vestimenta.get()
         quantidade_tipo_vestimenta = self.variable_entry_quantidade.get()
-        # data_inclusao = datetime.now().strftime("%d/%m/%y")
         db.insert_tipo_vestimenta(descricao, quantidade_tipo_vestimenta)
         print("Tipo vestimenta Cadastrado com sucesso!")
 
@@ -98,7 +132,7 @@ class GerenciadorTelas:
         self.telas = dict()
 
         for ClasseTela in (TelaInicial, TelaCadastrarHabilidades, TelaCadastrarTipoUso, 
-                           TelaCadastrarTipoVestimenta, TelaCadastrarTipoLavagem):
+                           TelaCadastrarTipoVestimenta, TelaCadastrarTipoLavagem, TelaCadastrarTecido):
             objeto_tela = ClasseTela(parent, self)
             self.telas[ClasseTela.__name__] = objeto_tela
             objeto_tela.grid(row=0, column=0, sticky="nsew")
