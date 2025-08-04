d74dba5a90f6b1c488ac2e9e76211833ca09225b
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-08-04 20:02:31 -0300

    take datetime.now() when registering TipoUso

--- tkinter/tela_inicial.py
+++ tkinter/tela_inicial.py
@@ -50,25 +50,15 @@ class TelaCadastrarTipoUso(ttk.Frame):
         self.entry_descricao_tipo_uso = ttk.Entry(self, textvariable=self.variable_entry_descricao_tipo_uso)
         self.entry_descricao_tipo_uso.grid(row=1, column=0)
 
-        self.label_data_inclusao = ttk.Label(self, text="Data Inclusao")
-        self.label_data_inclusao.grid(row=2, column=0)
-
-        self.variable_entry_data_inclusao = StringVar()
-        self.entry_data_inclusao = ttk.Entry(self, textvariable=self.variable_entry_data_inclusao)
-        self.entry_data_inclusao.grid(row=3, column=0)
-        # self.variable_entry_descricao_habilidade = StringVar()
-        # self.entry_descricao_habilidade = ttk.Entry(self, textvariable=self.variable_entry_descricao_habilidade)
-        # self.entry_descricao_habilidade.grid(row=3, column=0)
-
         self.botao_cadastrar_descricao_tipo_uso = ttk.Button(self, text="Cadastrar", command=self.cadastrar_tipo_uso)
-        self.botao_cadastrar_descricao_tipo_uso.grid(row=4, pady=10)
+        self.botao_cadastrar_descricao_tipo_uso.grid(row=2, pady=10)
           
         self.botao_consultar_habilidades = ttk.Button(self, text="Voltar para tela inicial", command=self.voltar_tela_inicial)
-        self.botao_consultar_habilidades.grid(row=5, pady=10)
+        self.botao_consultar_habilidades.grid(row=3, pady=10)
 
     def cadastrar_tipo_uso(self):
         descricao = self.variable_entry_descricao_tipo_uso.get()
-        data_inclusao = self.variable_entry_data_inclusao.get()
+        data_inclusao = datetime.now().strftime("%d/%m/%Y")
         db.insert_tipo_uso(descricao, data_inclusao)
         print("tipo uso inserido com sucesso")
 
