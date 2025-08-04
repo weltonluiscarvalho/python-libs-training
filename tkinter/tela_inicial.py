66b7f5f1cce00d8e10e171a56b7223e857ad281b
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-08-04 20:08:36 -0300

    add CadastrarTipoLavagem screen

--- tkinter/tela_inicial.py
+++ tkinter/tela_inicial.py
@@ -3,6 +3,33 @@ from tkinter import ttk
 import db
 from datetime import datetime
 
+class TelaCadastrarTipoLavagem(ttk.Frame):
+    def __init__(self, parent, gerenciador):
+        super().__init__(parent)
+
+        self.gerenciador = gerenciador
+        self.label_descricao_tipo_lavagem = ttk.Label(self, text="Tipo da lavagem")
+        self.label_descricao_tipo_lavagem.grid(row=0, column=0)
+
+        self.variable_entry_descricao_tipo_lavagem = StringVar()
+        self.entry_descricao_tipo_lavagem = ttk.Entry(self, textvariable=self.variable_entry_descricao_tipo_lavagem)
+        self.entry_descricao_tipo_lavagem.grid(row=1, column=0)
+
+        self.botao_cadastrar_descricao_tipo_uso = ttk.Button(self, text="Cadastrar", command=self.cadastrar_tipo_lavagem)
+        self.botao_cadastrar_descricao_tipo_uso.grid(row=4, pady=10)
+          
+        self.botao_consultar_habilidades = ttk.Button(self, text="Voltar para tela inicial", command=self.voltar_tela_inicial)
+        self.botao_consultar_habilidades.grid(row=5, pady=10)
+
+    def cadastrar_tipo_lavagem(self):
+        descricao = self.variable_entry_descricao_tipo_lavagem.get()
+        data_inclusao = datetime.now().strftime("%d/%m/%Y")
+        db.insert_tipo_lavagem(descricao, data_inclusao)
+        print("Tipo lavagem Cadastrado com sucesso!")
+
+    def voltar_tela_inicial(self):
+        self.gerenciador.alterar_tela_atual('TelaInicial')
+
 class TelaCadastrarTipoVestimenta(ttk.Frame):
     def __init__(self, parent, gerenciador):
         super().__init__(parent)
@@ -70,7 +97,8 @@ class GerenciadorTelas:
     def __init__(self, parent) -> None:
         self.telas = dict()
 
-        for ClasseTela in (TelaInicial, TelaCadastrarHabilidades, TelaCadastrarTipoUso, TelaCadastrarTipoVestimenta):
+        for ClasseTela in (TelaInicial, TelaCadastrarHabilidades, TelaCadastrarTipoUso, 
+                           TelaCadastrarTipoVestimenta, TelaCadastrarTipoLavagem):
             objeto_tela = ClasseTela(parent, self)
             self.telas[ClasseTela.__name__] = objeto_tela
             objeto_tela.grid(row=0, column=0, sticky="nsew")
