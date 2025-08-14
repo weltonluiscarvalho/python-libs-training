986fe6e9d5f1ff8fdfd8b23ca513ff4559150eab
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-08-13 21:47:32 -0300

    add 1 more space

--- tkinter/main.py
+++ tkinter/main.py
@@ -5,6 +5,7 @@ import ttkbootstrap.constants as ttkb_const
 from tela_inicial import GerenciadorTelas
 
 
+
 root = ttkb.Window(themename='superhero')
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
